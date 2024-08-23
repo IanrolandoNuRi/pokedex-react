3444197127dfd97763e21e28db04791126c49e56
Gilberth Nunez <gilberthnunez@Gilberths-Laptop.local>
2024-08-23 15:53:15 -0500

    tool-tip

--- src/components/atoms/pokemon-types-circles/pokemon-types-circles.tsx
+++ src/components/atoms/pokemon-types-circles/pokemon-types-circles.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./pokemon-types-circles.module.css";
 import {typeColors} from "../../../types/typeColors";
 
@@ -7,12 +7,67 @@ interface ColorCirclesProps {
 }
 
 const ColorCircles: React.FC<ColorCirclesProps> = ({ circles }) => {
+    const [isCircleHovered, setIsCircleHovered] = useState(false);
+    const [isToolTipHovered, setIsToolTipHovered] = useState(false);
+
+    const handleMouseEnterCircle = () => {
+      setIsCircleHovered(true);
+    };
+  
+    const handleMouseLeaveCircle = () => {
+      setIsCircleHovered(false);
+    };
+    
+    const handleMouseEnterToolTip = () => {
+      setIsToolTipHovered(true);
+    };
+  
+    const handleMouseLeaveToolTip = () => {
+      setIsToolTipHovered(false);
+    };
+
     return (
-        <div className={styles.circles}>
-            {circles.map((circleColor, index) => (
-                <span key={index} className={styles.circle} style={{ backgroundColor: typeColors[circleColor] }}></span>
-            ))}
+        <div>
+            <div className={styles.circles}
+            onMouseEnter={handleMouseEnterCircle}
+            onMouseLeave={handleMouseLeaveCircle}
+            >
+                {circles.map((circleColor, index) => (
+                    <span key={index} className={styles.circle} style={{ backgroundColor: typeColors[circleColor] }}></span>
+                ))}
+            </div>
+            {(isCircleHovered || isToolTipHovered) && (
+        <div
+        className={styles.hoveredElement}
+        onMouseEnter={handleMouseEnterToolTip}
+        onMouseLeave={handleMouseLeaveToolTip}
+        style={{border: '1px solid #ccc', padding: '10px', position: "absolute", backgroundColor: "white", zIndex:4}}>
+          <table>
+            <thead>
+              <tr>
+                <th>Color</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+                {circles.map((circleColor, index) => (
+                    <tr>
+                        <td>
+                        <span 
+                            key={index}
+                            className={styles.circle}
+                            style={{ 
+                                backgroundColor: typeColors[circleColor]
+                            }}></span>
+                        </td>
+                        <td>{circleColor}</td>
+                    </tr>
+                ))}
+            </tbody>
+          </table>
         </div>
+      )}
+    </div>
     );
 };
 
